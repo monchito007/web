54b2efe17588c535c4e1fa2d48d6d7fa0a90de6b
monchito007 <monchito007@gmail.com>
2014-01-18 22:28:17 +0100

    primera subida

---
+++ sites/default/files/languages/ca_ZfE83YwlBnA9b_fwmENaj4YACLfSXjhtRoWTN10u8MA.js
@@ -0,0 +1,1 @@
+Drupal.locale = { 'pluralFormula': function ($n) { return Number(($n>1)); }, 'strings': {"":{"Loading":"Carregant","Hide":"Oculta","Show":"Mostra","Configure":"Configura","Done":"Fet"}} };
